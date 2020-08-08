b78b6d2f27d913914494d971026db06b96ea7e37
ahmed-kaif <kaifkhan.rcs@gmail.com>
2020-08-08 23:17:02 +0600

    urllib to retrive web pages

---
+++ urlib1.py
@@ -0,0 +1,9 @@
+# using urllib to retrive web pages
+import urllib.request
+import urllib.parse
+import urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+for line in fhand:
+    print(line.decode().strip())
